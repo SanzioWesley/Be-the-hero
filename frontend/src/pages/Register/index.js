c07da70be821b823249a1608f804b3a3aad4e569
SanzioWesley <sanziowesley@hotmail.com>
2020-03-26 02:04:53 -0300

    Front End

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+
+import './styles.css'
+
+import logoImg from '../../assets/logo.svg'
+
+export default function Register() {
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be the Heroe" />
+
+                    <h1>Cadastro</h1>
+                    <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrar os casos da sua ONG</p>
+
+                    <Link className="back-link" to="/register">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </section>
+            </div>
+        </div>
+    )
+}
